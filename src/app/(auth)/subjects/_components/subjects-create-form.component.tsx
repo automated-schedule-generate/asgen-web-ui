c750a62b9efcd4430741f01101cc559917e8f523
RodriguesClaudiane <claudianerodrigues365@gmail.com>
2026-05-20 23:10:16 -0300

    fix: adicionando o callback na função que puxa os dados

--- src/app/(auth)/subjects/_components/subjects-create-form.component.tsx
+++ src/app/(auth)/subjects/_components/subjects-create-form.component.tsx
@@ -34,7 +34,6 @@ export default function SubjectsCreateFormComponent({
     reset,
     formState: { isValid },
   } = useFormWithZod(subjectSchema, {
-    mode: 'onChange',
     defaultValues: {
       is_optional: false,
     },
