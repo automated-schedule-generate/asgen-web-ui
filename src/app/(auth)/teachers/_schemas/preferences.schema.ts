778aa97f85f04eabea5888057035805f1a844b3f
RodriguesClaudiane <claudianerodrigues365@gmail.com>
2026-06-03 18:08:07 -0300

    feat: enviando as preferências para o banco de dados

--- src/app/(auth)/teachers/_schemas/preferences.schema.ts
+++ src/app/(auth)/teachers/_schemas/preferences.schema.ts
@@ -2,9 +2,7 @@ import { z } from 'zod';
 
 export const preferencesSchema = z.object({
   special_need: z.boolean(),
-  description_special_need: z.string().min(10, {
-    message: 'Descrição da necessidade especial é obrigatória',
-  }),
+  description_special_need: z.string().optional(),
   observation: z.string().optional(),
 });
 
